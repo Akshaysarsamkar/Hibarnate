885c3f556b2a7caf6881ccff29f0311a3209e075
Akshaysarsamkar <sarsamkarakshay@gmail.com>
2024-05-01 09:10:33 -0700

    how to read image

---
+++ annotationuse/src/main/java/com/annotationuse/Address.java
@@ -0,0 +1,115 @@
+package com.annotationuse;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+
+@Entity
+@Table(name="student_address")
+public class Address {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="a_id")
+	private int id;
+	
+	@Column(length = 20)
+	private String city;
+	
+	@Column(length = 50)
+	private String street;
+	private boolean isopen;
+	
+	@Temporal(TemporalType.DATE)
+	private Date adddate;
+	
+	@Transient
+	private double x;
+	
+	@Lob
+	private byte[] image;
+	
+	public Address() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Address(int id, String city, String street, boolean isopen, Date adddate, double x, byte[] image) {
+		super();
+		this.id = id;
+		this.city = city;
+		this.street = street;
+		this.isopen = isopen;
+		this.adddate = adddate;
+		this.x = x;
+		this.image = image;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public boolean isIsopen() {
+		return isopen;
+	}
+
+	public void setIsopen(boolean isopen) {
+		this.isopen = isopen;
+	}
+
+	public Date getAdddate() {
+		return adddate;
+	}
+
+	public void setAdddate(Date adddate) {
+		this.adddate = adddate;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public byte[] getImage() {
+		return image;
+	}
+
+	public void setImage(byte[] image) {
+		this.image = image;
+	}
+	
+	
+
+}
--- annotationuse/src/main/java/com/annotationuse/App.java
+++ annotationuse/src/main/java/com/annotationuse/App.java
@@ -1,12 +1,18 @@
 package com.annotationuse;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Date;
+
+import javax.imageio.stream.FileImageInputStream;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class App {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.out.println("Hello World!");
 
 		Configuration cfg = new Configuration();
@@ -20,17 +26,35 @@ public class App {
 		Transaction t = session.beginTransaction();
 
 		Collage c = new Collage();
-		c.setC_id(891);
+		c.setC_id(9891);
 		c.setC_name("Bjs Collage");
 		c.setAdds("wagholi,pune");
 		c.setTimepass(85.85f);
-		
+
+		// creating addess object class
+
+		Address a = new Address();
+		a.setStreet("street1");
+		a.setCity("pune");
+		a.setIsopen(true);
+		a.setAdddate(new Date());
+		a.setX(52.89);
+
+		// Reading image
+		FileInputStream file = new FileInputStream("src/main/java/bg.png");
+		byte[] data = new byte[file.available()];
+		file.read();
+		a.setImage(data);
+
 		System.out.println(c);
 
 		session.save(c);
+		session.save(a);
+
 		t.commit();
 		session.close();
-		
+
+		System.out.println("done");
 
 	}
 }
